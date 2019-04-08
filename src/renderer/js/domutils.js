c7a5f1f1b1c3ca0db61d89ed713cb6f8637eaed9
zombieleet <zombieleetnca@gmail.com>
2019-04-08 23:55:37 +0100

    implement toast notifications

--- src/renderer/js/domutils.js
+++ src/renderer/js/domutils.js
@@ -2,12 +2,23 @@
 
 module.exports.toast = ( { text , createAfter, deleteAfter } ) => {
 
+    const animateProperties = [
+        { right: "5px"  },
+        { right: "10px" },
+        { right: "15px" },
+        { right: "20px" },
+        { right: "25px" },
+        { right: "30px" },
+        { right: "35px" },
+        { right: "40px" }
+    ];
+
     setTimeout( () => {
-        
+
         const toastParent = document.createElement("div");
         const toastClose  = document.createElement("p");
         const toastText   = document.createElement("p");
-    
+
 
         toastParent.classList.add("toast-parent");
         toastClose.classList.add("toast-close");
@@ -15,16 +26,31 @@ module.exports.toast = ( { text , createAfter, deleteAfter } ) => {
         toastClose.classList.add("fa-times");
         toastText.classList.add("class", "toast-text");
 
-        toastParent.appendChild(toastClose);
+        toastClose.addEventListener("click", () => toastParent.remove());
+
         toastParent.appendChild(toastText);
+        toastParent.appendChild(toastClose);
 
         toastText.textContent = text;
 
         document.body.appendChild(toastParent);
 
         setTimeout( () => {
-            toastParent.remove();
+            toastParent.animate( Array.from(animateProperties).reverse(), {
+                duration: 100,
+                fill: "forwards"
+            });
+
         } , 5000 );
-        
+
+        setTimeout( () => {
+            toastParent.remove();
+        },5100);
+
+        toastParent.animate( animateProperties , {
+            duration: 100,
+            fill: "forwards"
+        });
+
     }, createAfter );
 };
