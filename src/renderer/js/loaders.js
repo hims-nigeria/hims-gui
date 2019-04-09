5b95c45c545077d105d84b7f89ca9be7a8eb58ec
zombieleet <zombieleetnca@gmail.com>
2019-04-09 15:25:46 +0100

    show toast when network is not avaiable or when network is avaiable

--- src/renderer/js/loaders.js
+++ src/renderer/js/loaders.js
@@ -1,24 +1,26 @@
 ; ( () => {
-    
+
     "use strict";
+
+    const { remote: { getCurrentWindow , app } } = require("electron");
 
     const { checkForInternet } = require("../js/utils.js");
     const { toast } = require("../js/domutils.js");
 
     window.addEventListener("DOMContentLoaded", async () => {
-        
+
         const id = setInterval( () => {
             const spinDots = document.querySelector(".spin-dots");
             const temp = spinDots.innerHTML;
             spinDots.innerHTML = temp;
         } , 6000);
-        
+
         checkForInternet((res,error) => {
-            clearInterval(id);
             if ( error ) {
                 toast({ text: "Yikes. You are not Connected to the Internet" , createAfter : 0 });
-                toast({ text: "Loading information from local database" , createAfter: 5000 });
-                return;
+                //toast({ text: "Loading information from local database" , createAfter: 5000 });
+            } else {
+                toast({ text: "This device is connected to the internet" , createAfter : 0 });
             }
             console.log("Connected");
         });
