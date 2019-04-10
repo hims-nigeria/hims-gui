416536e277c3cd6e66bb47454976c1607f672705
zombieleet <zombieleetnca@gmail.com>
2019-04-10 04:40:27 +0100

    constants

---
+++ src/renderer/js/constants.js
@@ -0,0 +1,9 @@
+"use strict";
+
+const { remote: { app } } = require("electron");
+
+const APP_PATH = app.getAppPath();
+const SUBDIR   = "src/renderer/html";
+
+module.exports.LOGIN_URL = `file://${APP_PATH}/${SUBDIR}/login.html`;
+module.exports.DASHBOARD_URL = `file://${APP_PATH}/${SUBDIR}/dashboard.html`;
