0b6bc00997664e75d35a760329343a8e675f4bb8
zombieleet <zombieleetnca@gmail.com>
2019-04-10 04:39:06 +0100

    add support for making api call to login to automatically login to platform in login.js

--- src/renderer/js/loaders.js
+++ src/renderer/js/loaders.js
@@ -6,25 +6,39 @@
 
     const { checkForInternet } = require("../js/utils.js");
     const { toast } = require("../js/domutils.js");
+    const { getDashboard } = require("../js/requests.js");
+
+    const { DASHBOARD_URL , LOGIN_URL } = require("../js/constants.js");
 
     window.addEventListener("DOMContentLoaded", async () => {
 
         const id = setInterval( () => {
+
             const spinDots = document.querySelector(".spin-dots");
             const temp = spinDots.innerHTML;
             spinDots.innerHTML = temp;
         } , 6000);
 
-        checkForInternet((res,error) => {
+        checkForInternet( async (res,error) => {
+
             if ( error ) {
                 toast({ text: "Yikes. You are not Connected to the Internet" , createAfter : 0 });
                 //toast({ text: "Loading information from local database" , createAfter: 5000 });
             } else {
                 toast({ text: "This device is connected to the internet" , createAfter : 0 });
             }
-            clearInterval(id);
-            getCurrentWindow().webContents.loadURL(`file://${app.getAppPath()}/src/renderer/html/user-section.html`);
 
+            setTimeout( async () => {
+
+                clearInterval(id);
+
+                const result = await getDashboard({
+                    nextUrl  :  LOGIN_URL
+                });
+
+                if ( result ) getCurrentWindow().webContents.loadURL(DASHBOARD_URL);
+
+            },10000);
         });
     });
 
