58b23e9c534b5affcc16651857a6c3d803c93e78
zombieleet <zombieleetnca@gmail.com>
2019-04-13 04:18:36 +0100

    create offline db schema for healthFacility and sessionObject

---
+++ src/renderer/js/db.js
@@ -0,0 +1,18 @@
+"use strict";
+
+const Dexie = require("dexie");
+const hospitalDb = new Dexie("HospitalDB");
+
+
+hospitalDb.version(1).stores({
+    healthFacility: "++id, &healthFacilityId, &email",
+    /**
+       sessionObject will contain an email address
+       the email address will be used to map to the current logged in user
+       put method will be used to replace whatever is in the session object
+       during registration or logging in
+     **/
+    sessionObject: "id,&email"
+});
+
+module.exports = hospitalDb;
