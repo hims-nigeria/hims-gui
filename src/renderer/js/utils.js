22261969012d59aef4b7dec5d1d02fba4ef78744
zombieleet <zombieleetnca@gmail.com>
2019-04-13 04:23:38 +0100

    creation of hsahPassword , comparePassword and createExternalId modules

--- src/renderer/js/utils.js
+++ src/renderer/js/utils.js
@@ -2,5 +2,24 @@
 
 const util = require("util");
 const https = require("https");
+const bcrypt = require("bcrypt");
 
 module.exports.checkForInternet = async cb => https.get("https://google.com", res => cb(res,null)).on("error", e => cb(null,e) );
+
+module.exports.hashPassword = async (pwd, cpwd) => {
+    if ( pwd !== cpwd ) return false;
+    try { return await bcrypt.hash( pwd , 10 ); } catch(ex) { return ex; }
+};
+
+module.exports.comparePassword = async ( plaintextPwd, hashedPwd )  => {
+    try {
+        return (await bcrypt.compare( plaintextPwd, hashedPwd ));
+    } catch(ex) {
+        return ex;
+    }
+};
+
+module.exports.createExternalId = (...criteria) => {
+    const crypto = require("crypto");
+    return crypto.createHash("sha1").update(criteria.join("")).digest("hex");
+};
