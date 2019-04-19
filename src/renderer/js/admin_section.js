4fa957056ad85191c8db2c5a54513abee4dd6630
zombieleet <zombieleetnca@gmail.com>
2019-04-19 01:08:44 +0100

    rendering of dashboard reports to the dom

--- src/renderer/js/admin_section.js
+++ src/renderer/js/admin_section.js
@@ -29,8 +29,42 @@
 
         if ( ! result ) return;
 
-        // append to dom
-        console.log(result);
+        const reportUl = document.createElement("ul");
+
+        reportUl.setAttribute("class", "dashboard-list");
+
+        let idx = 0;
+        
+        Object.keys(result.dashboardInfo).sort().forEach( x => {
+
+            const li = document.createElement("li");
+            const p  = document.createElement("p");
+            const i  = document.createElement("i");
+
+            const pText  = document.createElement("p");
+
+            li.classList.add("dashboard-item");
+            li.classList.add(`dashboard-item-${idx++}`);
+            p.setAttribute("class", "dashboard-item-count");
+            pText.setAttribute("class", "dashboard-item-name");
+
+            p.textContent = result.dashboardInfo[x];
+            pText.textContent = x.replace(/^./, x[0].toUpperCase());
+
+            li.appendChild(pText);
+            li.appendChild(p);
+            li.appendChild(i);
+            
+            reportUl.appendChild(li);
+        });
+
+        const userRole = document.querySelector(".user-role");
+        const userName = document.querySelector(".user-name");
+        const sectionNavOps = document.querySelector(".section-nav-operation");
+        
+        userRole.textContent = result.role;
+        userName.textContent = result.fullName;
+        sectionNavOps.appendChild(reportUl);
     });
 
 })();
--- src/renderer/js/db.js
+++ src/renderer/js/db.js
@@ -6,6 +6,16 @@ const hospitalDb = new Dexie("HospitalDB");
 
 hospitalDb.version(1).stores({
     healthFacility: "++id, &healthFacilityId, &email",
+    accountants: "++id, &accontantId, &phoneNumber, &email",
+    departments: "++id",
+    doctors: "++id, &doctorId, &phoneNumber, &email",
+    interns: "++id, &internId, &phoneNumber, &email",
+    laboratorists: "++id, &laboratoristId, &phoneNumber, &email",
+    nurses: "++id, &nurseId, &phoneNumber, &email",
+    patients: "++id, &patientId, &phoneNumber, &email",
+    pharamcists: "++id, &pharmacistId, &phoneNumber, &email",
+    transactions: "++id",
+    
     /**
        sessionObject will contain an email address
        the email address will be used to map to the current logged in user
