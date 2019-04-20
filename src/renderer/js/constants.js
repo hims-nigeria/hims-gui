88402c6aff0cb29af0ae0143b7cab1d9a743b771
zombieleet <zombieleetnca@gmail.com>
2019-04-21 00:54:46 +0100

    add ADD_NURSE_URL and NOT_MAIN_WINDOW_URL constants

--- src/renderer/js/constants.js
+++ src/renderer/js/constants.js
@@ -8,3 +8,5 @@ const SUBDIR   = "src/renderer/html";
 module.exports.LOGIN_URL = `file://${APP_PATH}/${SUBDIR}/login.html`;
 module.exports.ADMIN_URL = `file://${APP_PATH}/${SUBDIR}/admin_section.html`,
 module.exports.REGISTER_URL = `file://${APP_PATH}/${SUBDIR}/register.html`;
+module.exports.ADD_NURSE_URL = `file://${APP_PATH}/${SUBDIR}/admin/admin_nurse.html`;
+module.exports.NOT_MAIN_WINDOW_URL = `file://${APP_PATH}/${SUBDIR}/not_main_window.html`;
